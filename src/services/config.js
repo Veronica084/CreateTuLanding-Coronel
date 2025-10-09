58df164f463983fd874cf59073810d7e3e6b6707
Veronica084 <nicavero084@gmail.com>
2025-10-09 09:45:36 -0300

    primer commit

--- src/services/config.js
+++ src/services/config.js
@@ -26,12 +26,12 @@ export const db = getFirestore(app)
 //------------------------------------------------------------------------------------
 
 const misProductos=[
-    {id: "1", nombre:"Multímetro", precio:20000, img: "../public/imag/img-elect/multimetro.jpg", idCat: "medición", stock: 10},
-    {id: "2", nombre:"Telurómetro", precio:100000, img:"../public/imag/img-elect/telurometro.jpg", idCat:"medición", stock: 15},
-    {id: "3", nombre:"Pinza Amperométrica", precio:29000, img:"../public/imag/img-elect/amperometrica.jpg", idCat:"medición", stock:10},
-    {id: "4", nombre:"Cable unipolar 2.5 X 100MTS", precio:80000, img:"../public/imag/img-elect/cable.jpg", idCat: "conductores", stock:10},
-    {id: "5", nombre:"Cinta pasacable", precio:10000, img:"../public/imag/img-elect/cinta.jpg", idCat: "herramientas", stock:10},
-    {id: "6", nombre:"Interruptor Termomagnético", precio:25000, img:"../public/imag/img-elect/termomagnetica.jpg", idCat: "protección", stock: 10},
+    {id: "1", nombre:"Multímetro", precio:20000, img: "../imag/img-elect/multimetro.jpg", idCat: "medición", stock: 10},
+    {id: "2", nombre:"Telurómetro", precio:100000, img:"../imag/img-elect/telurometro.jpg", idCat:"medición", stock: 15},
+    {id: "3", nombre:"Pinza Amperométrica", precio:29000, img:"../imag/img-elect/amperometrica.jpg", idCat:"medición", stock:10},
+    {id: "4", nombre:"Cable unipolar 2.5 X 100MTS", precio:80000, img:"../imag/img-elect/cable.jpg", idCat: "conductores", stock:10},
+    {id: "5", nombre:"Cinta pasacable", precio:10000, img:"../imag/img-elect/cinta.jpg", idCat: "herramientas", stock:10},
+    {id: "6", nombre:"Interruptor Termomagnético", precio:25000, img:"../imag/img-elect/termomagnetica.jpg", idCat: "protección", stock: 10},
 ]
 
 
