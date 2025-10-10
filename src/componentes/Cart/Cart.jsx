fee28a21b02b6905a36d21d386804e1f1bf664c0
Veronica084 <nicavero084@gmail.com>
2025-10-10 01:13:29 -0300

    electriciti

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { CarritoContext } from "../../context/CarritoContext"
 import { Link } from "react-router-dom"
 import CartItem from "../CartItem/CartItem"
-import "./cart.css"
+import "./Cart.css"
 
 
 const Cart = () => {
